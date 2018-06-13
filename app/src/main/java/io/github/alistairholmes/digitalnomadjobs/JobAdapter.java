1443f0faf43f4ca74b4bce6069ce625816fd04d1
alistair <alistairholmes95@gmail.com>
2018-06-13 20:28:49 +0200

    Richard back to the rescue! Added line to remove first row that was blank in the adapter.

--- app/src/main/java/io/github/alistairholmes/digitalnomadjobs/JobAdapter.java
+++ app/src/main/java/io/github/alistairholmes/digitalnomadjobs/JobAdapter.java
@@ -4,14 +4,11 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -67,7 +64,7 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.ViewHolder> {
 
     // Pass in a the Job constructor
     public JobAdapter(List<Job> job, OnItemClickListener listener) {
-        jobs = job;
+        jobs=job.subList(1, job.size());
         this.mListener = listener;
     }
 
