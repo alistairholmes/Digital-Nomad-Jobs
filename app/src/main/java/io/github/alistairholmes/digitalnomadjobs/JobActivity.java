92f658b7fd1999218ceb08b5d445148d1221ad97
alistair <alistairholmes95@gmail.com>
2018-06-15 23:44:09 +0200

    OnJobClick opens custom themed browser to view url.

--- app/src/main/java/io/github/alistairholmes/digitalnomadjobs/JobActivity.java
+++ app/src/main/java/io/github/alistairholmes/digitalnomadjobs/JobActivity.java
@@ -1,8 +1,9 @@
 package io.github.alistairholmes.digitalnomadjobs;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -11,7 +12,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +19,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,21 +29,18 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class JobActivity extends AppCompatActivity {
-    private ProgressBar mProgressBar;
     private TextView mNoInternetConnectionTv;
     private ImageView mNoWifiConnectionIv;
 
     private SwipeRefreshLayout swipeContainer;
 
-    private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
 
     public RecyclerView.Adapter mAdapter;
     public RecyclerView mainRecyclerView;
     public String url = "https://remoteok.io/remote-jobs.json";
-    public static final String LOGO_URL = "logoUrl";
 
-    public List<Job> jobs;
+
 
     private static final String LOG_TAG = JobActivity.class.getName();
 
@@ -53,7 +49,6 @@ public class JobActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar_loader);
         mNoInternetConnectionTv = findViewById(R.id.no_internet_connection_tv);
         mNoWifiConnectionIv = findViewById(R.id.no_wifi_connection_imageview);
         mainRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_main);
@@ -92,7 +87,6 @@ public class JobActivity extends AppCompatActivity {
                 // Code to refresh the list here.
                 try {
                     loadJobData();
-                    mProgressBar.setVisibility(View.INVISIBLE);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -121,7 +115,6 @@ public class JobActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call call, IOException e) {
                 call.cancel();
-                mProgressBar.setVisibility(View.INVISIBLE);
                 mNoInternetConnectionTv.setText(R.string.no_internet_connection);
                 mNoWifiConnectionIv.setVisibility(View.VISIBLE);
 
@@ -138,20 +131,22 @@ public class JobActivity extends AppCompatActivity {
                 JobActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mProgressBar.setVisibility(View.INVISIBLE);
 
-                            Gson gson = new GsonBuilder().create();
-                            List<Job> jobs = new ArrayList<Job>();
-                            jobs = Arrays.asList(gson.fromJson(jsonResponse, Job[].class));
+                        Gson gson = new GsonBuilder().create();
+                        List<Job> jobs = Arrays.asList(gson.fromJson(jsonResponse, Job[].class));
 
                         mAdapter = new JobAdapter(jobs, new JobAdapter.OnJobClickListener() {
                             @Override
                             public void onJobClick(Job job) {
                                 if (job.getUrl() != null) {
-                                    Uri jobURL = Uri.parse(job.getUrl());
-                                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                                    intent.setData(jobURL);
-                                    startActivity(intent);
+                                    // Use a CustomTabsIntent.Builder to configure CustomTabsIntent.
+                                    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                                    // set toolbar color and set custom actions before invoking build()
+                                    builder.setToolbarColor(ContextCompat.getColor(JobActivity.this, R.color.colorAccent));
+                                    // Once ready, call CustomTabsIntent.Builder.build() to create a CustomTabsIntent
+                                    CustomTabsIntent customTabsIntent = builder.build();
+                                    // and launch the desired Url with CustomTabsIntent.launchUrl()
+                                    customTabsIntent.launchUrl(JobActivity.this, Uri.parse(job.getUrl()));
                                 } else {
                                     Toast.makeText(JobActivity.this, "Sorry no URL is available for this job at the moment. Please try again later",
                                             Toast.LENGTH_SHORT).show();
