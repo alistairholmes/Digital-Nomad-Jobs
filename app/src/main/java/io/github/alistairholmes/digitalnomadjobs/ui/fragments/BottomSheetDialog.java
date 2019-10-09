c5d8c75ebe4e7f5f60661aa4e32a0d525ad9ff21
alistair <alistairholmes95@gmail.com>
2019-10-09 10:28:45 +0200

    Setting up the bottom sheet fragment.

--- app/src/main/java/io/github/alistairholmes/digitalnomadjobs/ui/fragments/BottomSheetDialog.java
+++ app/src/main/java/io/github/alistairholmes/digitalnomadjobs/ui/fragments/BottomSheetDialog.java
@@ -1,6 +1,7 @@
 package io.github.alistairholmes.digitalnomadjobs.ui.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -17,6 +18,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import io.github.alistairholmes.digitalnomadjobs.R;
+import io.github.alistairholmes.digitalnomadjobs.ui.about.AboutActivity;
+import io.github.alistairholmes.digitalnomadjobs.ui.jobs.JobActivity;
 
 /**
  * <p>A fragment that shows a list of items as a modal bottom sheet.</p>
@@ -41,6 +44,18 @@ public class BottomSheetDialog extends com.google.android.material.bottomsheet.B
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false);
+        View v = inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false);
+
+        TextView javascriptTag = v.findViewById(R.id.btn_javascript);
+        javascriptTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AboutActivity.class));
+            }
+        });
+
+        return v;
     }
+
+
 }
